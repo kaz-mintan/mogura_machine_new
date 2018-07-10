2fcb31a9aa49bb802a7bda4d6c989b03c49a4ce3
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-07-10 17:00:31 +0900

    first commit

---
+++ sensors/distance/distance.ino
@@ -0,0 +1,24 @@
+void setup() {
+     Serial.begin(9600) ;          // 9600bpsでシリアル通信のポートを開きます
+ }
+void loop() {
+     int ans ;
+
+     ans = IDMread(0)  ;           // アナログピン０番のセンサー値を読込む
+     ans = map(ans,0,1023,0,500) ; // 読込んだ値から電圧値に変換する(0-5V)
+     ans = map(ans,50,220,50,4) ;  // 電圧値から距離に変換する(0.5-2.2V -> 50-4cm)
+     Serial.print(ans) ;           // シリアルモニターに表示させる
+     Serial.println("cm") ;
+     delay(500) ;                  // 500ms後に繰り返す
+ }
+// 赤外線測距モジュールから読み込む処理
+int IDMread(int PinNo) {
+     long ans ;
+     int i ;
+
+     ans = 0 ;
+     for (i=0 ; i < 100 ; i++) {
+          ans  = ans + analogRead(PinNo) ;   // 指定のアナログピンから読取ります
+     }
+     return ans/100 ;                        // 100回の平均値を返します
+}
---
+++ sensors/readme.md
@@ -0,0 +1,9 @@
+# sensors
+
+- contains .ino files to sense.
+
+# factors memo
+
+- [もぐちゃんのためのスプレッドシート](https://docs.google.com/spreadsheets/d/1Rqf6mQX8FMSqqRlIWYgFpAiD0sdwekIcol3oy6x3RW8/edit#gid=0)
+
+	- contains factor candidates to estimate emotion during mogura game.
